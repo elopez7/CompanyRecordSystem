712508498fedf5e859f73b46cc6765c283a2584f
Esteban Lopez <e.lopez7@hotmail.com>
2021-02-07 12:33:03 -0500

    First commit from Rider

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyRecordSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
